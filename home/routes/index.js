5b90df3eab99cc1cef443bb6be2f94e31c6660e5
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2015-10-23 11:53:46 -0700

    Trying to get a handle on this

--- home/routes/index.js
+++ home/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('FML');
+  console.log('*** FML ***');
   res.render('index', { title: 'Baldwin codes' });
 });
 
