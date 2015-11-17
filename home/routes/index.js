a860110b9af2dbb401c97fc9ad90f74e01315c41
sterlingbaldwin <sbaldwin4@mail.csuchico.edu>
2015-11-17 13:02:45 -0800

    messing with routes again

--- home/routes/index.js
+++ home/routes/index.js
@@ -1,40 +1,56 @@
 var express = require('express');
-var router = express.Router();	
+var router = express.Router();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-  	title: 'Baldwin codes' 
+  res.render('index', {
+  	title: 'Baldwin codes'
   });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('projects', { 
-  	title: 'Projects' 
+  res.render('projects', {
+  	title: 'Projects'
   });
 });
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('about', { 
-  	title: 'About' 
+  res.render('about', {
+  	title: 'About'
   });
 });
 
 /* GET robots page. */
 router.get('/robots', function(req, res, next) {
-  res.render('robots', { 
-  	title: 'Robot Projects' 
+  res.render('robots', {
+  	title: 'Robot Projects'
   });
 });
 
 /* GET Blog. */
 router.get('/blog', function(req, res, next) {
-  res.render('blog', { 
-  	title: 'Blog' 
+  res.render('blog', {
+  	title: 'Blog'
   });
 });
+
+/* GET Blog. */
+router.get('/wladder', function(req, res, next) {
+  res.render('wladder', {
+  	title: 'Wladder'
+  });
+});
+
+/* GET Blog. */
+router.get('/img', function(req, res, next) {
+  res.render('img', {
+  	title: 'Img'
+  });
+});
+
+
 
 module.exports = router;
